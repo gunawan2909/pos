4f850c170a648db1cdd325a3b027f51ab25f20e3
GUNAWAN PRASETYA <gunawan29092000@gmail.com>
2023-06-15 08:06:40 +0700

    geps

--- routes/web.php
+++ routes/web.php
@@ -49,7 +49,7 @@ Route::get('/karyawan', function () {
 })->name('karyawan');
 Route::get('/tes', function () {
     // Mail::to('gunawan@gmail.com')->send(new SendStruk(1));
-    dispatch(new StruckJob(1));
+    dispatch(new StruckJob(16));
 });
 
 Route::middleware('guest')->group(function () {
