c98cc2a62b666049e6f2d8eb1a04a8cfe5c6805d
GUNAWAN PRASETYA <gunawan29092000@gmail.com>
2023-06-17 12:09:14 +0700

    geps

---
+++ config/css-inliner.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Css Files
+    |--------------------------------------------------------------------------
+    |
+    | Css file of your style for your emails
+    | The content of these files will be added directly into the inliner
+    | Use absolute paths, ie. public_path('css/main.css')
+    |
+    */
+
+    'css-files' => ['https://ta.circlecafe.site/build/assets/app-9bcb82f7.css'],
+
+];
